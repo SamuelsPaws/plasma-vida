ac66f854f53223b51622508ee14b2d567c9f1eae
Samuel Baquero <samsantbaq@gmail.com>
2026-03-21 00:52:14 -0500

    mobile about update

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -2,31 +2,31 @@ const Footer = () => {
   return (
     <footer className='lg:h-[400px] px-6 lg:px-8 pb-6 lg:pb-8 bg-[#ececec]'>
         <div className="
-            w-full lg:h-full p-8 lg:p-0
-            flex flex-col lg:flex-row lg:justify-evenly items-center gap-8 lg:gap-0
+            w-full lg:h-full py-12 lg:py-0
+            flex flex-col lg:flex-row lg:justify-evenly items-center gap-16 lg:gap-0
             bg-maingold-dark-1 rounded-2xl"
         >
             <div className="flex flex-col gap-1 lg:gap-4 items-center lg:items-start">
-                <p className="mb-2 text-xl lg:text-2xl font-bold text-white-1">
+                <p className="mb-2 text-2xl lg:text-2xl font-bold text-white-1">
                     Contacto
                 </p>
-                <p className="text-sm lg:text-lg text-white-1/80">
+                <p className="text-md lg:text-lg text-white-1/80">
                     Teléfono: +593 97 877 4224
                 </p>
-                <p className="text-sm lg:text-lg text-white-1/80">
+                <p className="text-md lg:text-lg text-white-1/80">
                     E-mail: cpaciente1626@gmail.com
                 </p>
             </div>
             <div className="flex flex-col gap-1 lg:gap-4 items-center lg:items-start">
-                <p className="mb-2 text-xl lg:text-2xl font-bold text-white-1">
+                <p className="mb-2 text-2xl lg:text-2xl font-bold text-white-1">
                     Redes Sociales
                 </p>
-                <p className="text-sm lg:text-lg text-white-1/80">
+                <p className="text-md lg:text-lg text-white-1/80">
                     <i className="fa fa-instagram mr-2"></i><span>@plasmavidacenter</span>
                 </p>
             </div>
             <div className="flex flex-col gap-2 lg:gap-4 items-center">
-                <p className="mb-2 text-xl lg:text-2xl font-bold text-white-1">
+                <p className="mb-2 text-2xl lg:text-2xl font-bold text-white-1">
                     Ubicación
                 </p>
                 <p className="text-7xl text-white/50"><i className="fa fa-map-marker" aria-hidden="true"></i></p>
