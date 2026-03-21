8a37be0c4be9695c42635d038b596e352e4b7306
Samuel Baquero <samsantbaq@gmail.com>
2026-03-20 21:54:04 -0500

    mobile banner update

--- app/components/Banner.tsx
+++ app/components/Banner.tsx
@@ -6,8 +6,8 @@ import { Promotion } from "@/lib/models/promotion"
 const Banner = ({ promotions }: { promotions: Promotion[] }) => {
   return (
     <div className="
-        h-[200px] lg:h-[420px] p-8 lg:p-10 relative
-        grid grid-cols-[70%_1fr] lg:grid-cols-2 gap-0
+        h-[240px] lg:h-[420px] p-8 lg:p-10 relative
+        grid grid-cols-[80%_1fr] lg:grid-cols-2 gap-0
         bg-[url('/assets/hero-img.webp')] bg-cover lg:bg-position-[0_-42px]
         before:content-[''] before:absolute before:inset-0 before:bg-[linear-gradient(to_right,#0007_30%,#0002)]"
     >
@@ -16,7 +16,7 @@ const Banner = ({ promotions }: { promotions: Promotion[] }) => {
             <motion.h1
                 className="
                     relative
-                    text-3xl lg:text-7xl text-white-1 font-bold"
+                    text-4xl lg:text-7xl text-white-1 font-bold"
                 initial={{ opacity: 0, y: -10 }}
                 animate={{ opacity: 1, y: 0 }}
                 transition={{ duration: 0.6 }}
@@ -24,7 +24,7 @@ const Banner = ({ promotions }: { promotions: Promotion[] }) => {
                 Plasma Vida Center
             </motion.h1>
             <motion.p
-                className="mt-1 lg:mt-4 text-sm lg:text-2xl text-white-1 relative"
+                className="mt-1 lg:mt-4 text-md lg:text-2xl text-white-1 relative"
                 initial={{ opacity: 0, y: -10 }}
                 animate={{ opacity: 1, y: 0 }}
                 transition={{ duration: 0.6, delay: 0.3 }}
