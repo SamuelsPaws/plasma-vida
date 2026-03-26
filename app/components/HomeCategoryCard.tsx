8536dc8848b4465e05d560e7f01aafe7bdc555f9
Samuel Baquero <samsantbaq@gmail.com>
2026-03-26 01:04:49 -0500

    better layout home categories

--- app/components/HomeCategoryCard.tsx
+++ app/components/HomeCategoryCard.tsx
@@ -11,26 +11,41 @@ interface HomeCategoryCardProps {
 const HomeCategoryCard = ({ title, descriptionParagraphs, href, linkText, imageUrl }: HomeCategoryCardProps) => {
   return (
     <div className="lg:px-8">
-        <h4 className="mb-8 text-2xl lg:text-4xl text-sky-800 text-center font-bold">{title}</h4>
         {/* Div with 3 parts */}
         <div className="
             w-full
-            flex flex-col lg:flex-row gap-y-8 lg:gap-y-8 flex-wrap"
+            flex flex-col lg:flex-row
+            items-center gap-y-8 lg:gap-y-8 flex-wrap"
         >
-            {/* Image */}
+            {/* Div with title and image */}
             <div className="
-                lg:flex-1 w-full lg:w-auto h-[300px] lg:h-[400px] lg:min-w-[300px]
-                rounded-2xl overflow-hidden"
+                flex-1 lg:min-w-[300px]
+                flex flex-col items-center justify-center gap-2"
             >
-                <img
-                    src={imageUrl}
-                    className="w-full h-full object-contain"
-                    alt=""
-                />
+                {/* Title */}
+                <h4 className="
+                    text-2xl lg:text-4xl
+                    text-sky-800 text-center font-bold"
+                >
+                    {title}
+                </h4>
+                {/* Image */}
+                <div className="
+                    w-full lg:w-auto
+                    h-[300px] lg:h-auto
+                    lg:w-full lg:max-w-[340px] lg:aspect-square
+                    rounded-2xl overflow-hidden"
+                >
+                    <img
+                        src={imageUrl}
+                        className="w-full h-full object-contain"
+                        alt=""
+                    />
+                </div>
             </div>
             {/* Description */}
             <div className="
-                lg:flex-1 lg:min-w-[480px] px-6 lg:px-12
+                lg:flex-1 lg:min-w-[340px] px-6 lg:px-12
                 flex flex-col justify-center items-center gap-4
                 text-sky-800"
             >
@@ -39,13 +54,13 @@ const HomeCategoryCard = ({ title, descriptionParagraphs, href, linkText, imageU
                 )}
             </div>
             {/* Link */}
-            <div className="flex-1 grid place-content-center">
+            <div className="flex-1 self-stretch grid place-content-center">
                 <Link
                     href={href}
                     className="
-                        lg:min-w-[320px] px-4 lg:px-6 py-2 lg:py-4
-                        flex justify-center items-center
-                        text-md lg:text-xl text-white-1 bg-teal-500 rounded-full"
+                        lg:min-w-[300px] px-6 py-4
+                        bg-teal-600
+                        text-md lg:text-xl text-white-1 rounded-full"
                 >
                     {linkText}<i className="fa fa-arrow-right scale-90 ml-2"></i>
                 </Link>
