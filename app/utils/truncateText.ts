19db6163d07a917d5b6bd2e4af5fe42defef1695
Samuel Baquero <samsantbaq@gmail.com>
2026-04-08 14:16:42 -0500

    strip punct from metadata desc

--- app/utils/truncateText.ts
+++ app/utils/truncateText.ts
@@ -1,7 +1,10 @@
 export default function truncateText(text: string, maxLength: number): string {
-  if (text.length <= maxLength) return text;
+  if (text.length <= maxLength) return text
 
-  const truncated = text.slice(0, maxLength);
+  const truncated = text.slice(0, maxLength)
+  const noMidWord = truncated.slice(0, truncated.lastIndexOf(" "))
+  const lastChar = noMidWord.charAt(noMidWord.length - 1)
+  const stripPunct = lastChar === '.' || lastChar === ',' ? noMidWord.slice(0, noMidWord.length - 1) : noMidWord 
 
-  return truncated.slice(0, truncated.lastIndexOf(" ")) + "...";
+  return stripPunct + "...";
 }
